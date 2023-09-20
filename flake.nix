c0996ad166b22d2e3830cb30f905760c375d7282
0nyr <rascoussier.florian@gmail.com>
2023-09-20 14:20:37 +0200

    Refactoring and first packaging attempt.

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A python package for easy python programming for research projects.";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # import packages
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonPackages = pkgs.python311Packages;
+      in
+      {
+        # Dev environment
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            # the environment.
+            pythonPackages.python
+
+            # python packages
+            pythonPackages.python-dotenv
+            pythonPackages.psutil
+
+            # python dev packages
+            pythonPackages.pytest
+            pythonPackages.setuptools
+          ];
+
+          buildInputs = with pkgs; [ ];
+        };
+
+        # Python package definition
+        packages.${system}.research-base = pkgs.pythonPackages.buildPythonPackage rec {
+          pname = "research-base";
+          version = "0.1.0";
+
+          src = ./.;
+
+          propagatedBuildInputs = with pkgs.pythonPackages; [
+            # Add your dependencies here
+            python-dotenv
+          ];
+
+          meta = with pkgs.lib; {
+            description = "research-base python framework";
+            license = licenses.mit;
+          };
+        };
+      }
+    );
+}
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='research-base',
+    version='0.0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # dependencies
+        'python-dotenv',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.11',
+    ],
+    author='Florian Rascoussier (0nyr), Clément Lahoche (Fitz35)',
+    author_email='darkvoldorious@gmail.com',
+    description='A python package for easy python programming for research projects.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
