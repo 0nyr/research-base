7ceaa53bbb71ba2bf61139cc8ba9c635db9f524d
Fitz35 <clement@lahoche.info>
2023-09-23 09:00:11 +0200

    add function to convert dict to csv value

--- src/research_base/utils/data_utils.py
+++ src/research_base/utils/data_utils.py
@@ -7,3 +7,10 @@ def count_labels(array: np.ndarray) -> dict:
     """
     unique, counts = np.unique(array, return_counts=True)
     return dict(zip(unique, counts))
+
+
+def dict_to_csv_value(dictionary: dict) -> str:
+    """
+    Convert a dictionary to a string, to be written as a value to a CSV file (wich doesn't have whitespace as delimiter).
+    """
+    return " ".join(["class-" + str(key) + "=" + str(value) for (key, value) in dictionary.items()])
